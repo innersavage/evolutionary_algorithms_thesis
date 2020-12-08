BugFix: np.float_power instead of np.float because of precision errors

test_functions/bukinf6.py
@@ -6,5 +6,5 @@ class BukinF6(TestFunction):
     def test_function(self, xx, a=10):
         if len(xx) != 2:
             raise Exception
-        f = 100 * np.sqrt(np.fabs(xx[1] - 0.01 * np.power(xx[0], 2))) + 0.01 * np.fabs(xx[0] + 10)
+        f = 100 * np.sqrt(np.fabs(xx[1] - 0.01 * np.float_power(xx[0], 2))) + 0.01 * np.fabs(xx[0] + 10)
         return f

test_functions/michalewicz.py
@@ -4,5 +4,5 @@ from .functionbase import TestFunction
 
 class Michalewicz(TestFunction):
     def test_function(self, xx, m=10):
-        f = - sum([np.sin(x) * np.power(np.sin(len(xx) * np.power(x, 2) / np.pi), 2 * m) for x in xx])
+        f = - sum([np.sin(x) * np.float_power(np.sin(len(xx) * np.float_power(x, 2) / np.pi), 2 * m) for x in xx])
         return f