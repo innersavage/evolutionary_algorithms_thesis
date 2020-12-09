6aceeaab88624f5ef4164477ecbf550377f7c0d8
luc <luc@slon.lotnik.net>
2020-12-09 16:30:01 +0100

    Added script for check completness

---
+++ check_completness.py
@@ -0,0 +1,31 @@
+import os
+
+
+how_many_results = 100
+combinations = 736
+
+path = os.path.dirname(os.path.abspath(__file__)) + '/results/'
+functions = ['Rastrigin', 'Michalewicz', 'Bukin6', 'Easom', 'Wolfe']
+
+for func in functions:
+    completed = 0
+    in_progress = 0
+    completness = 0
+    if os.path.isdir(path + func):
+        for i in range(1, combinations + 1):
+            if os.path.isfile(path + func + '/result_{}.csv'.format(i)):
+                with open(path + func + '/result_{}.csv'.format(i), 'r') as f:
+                    lines = len(f.readlines())
+                    if lines > how_many_results:
+                        completed += 1
+                        completness += 1
+                    else:
+                        in_progress += 1
+                        if lines > 0:
+                            completness += (lines - 1) / 100
+    print('# {} function progress'.format(func))
+    print('## Completed: {} In progress: {} Total: {} Completness: {:02.2f}%'.format(completed,
+                                                                                     in_progress,
+                                                                                     combinations,
+                                                                                     completness/combinations))
+
