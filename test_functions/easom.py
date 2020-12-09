864eaaf110b3e1573632eb5a4548a690e7a54848
luc <luc@slon.lotnik.net>
2020-12-09 22:09:56 +0100

    Huge mistake in Easom function

--- test_functions/easom.py
+++ test_functions/easom.py
@@ -6,5 +6,5 @@ class Easom(TestFunction):
     def test_function(self, xx):
         if len(xx) != 2:
             raise Exception
-        f = - np.cos(xx[0]) * np.cos(xx[1]) * np.exp(- np.float_power(xx[0] - np.pi, 2) - np.float_power(xx[1] - np.pi, 2))
+        f = - np.cos(xx[0]) * np.cos(xx[1]) * np.exp(- np.float_power(xx[0] - np.pi, 2) + np.float_power(xx[1] - np.pi, 2))
         return f
