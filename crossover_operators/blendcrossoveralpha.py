d8171ea61d989dda29c58999a3ee44419869f320
luc <luc@slon.lotnik.net>
2020-12-10 17:51:13 +0100

    Fixed Blend Crossover operators to handle cubic polynomials

--- crossover_operators/blendcrossoveralpha.py
+++ crossover_operators/blendcrossoveralpha.py
@@ -7,10 +7,10 @@ class BlendCrossoverAlpha(CrossoverBase):
     datatype = 'real'
 
     def crossover_function(self, ind, alpha=0.7, **kwargs):
-        x, y = [ind[0][0], ind[1][0]], [ind[1][0], ind[1][1]]
-        dx, dy = abs(x[0] - x[1]), abs(y[0] - y[1])
+        xy = [[j[i] for j in ind] for i in range(len(ind[0]))]
+        dd = [abs(i[0] - i[1]) for i in xy]
         new_ind = []
         for i in range(2):
-            new_ind.append([random.SystemRandom().uniform(min(x) - alpha * dx, max(x) + alpha * dx),
-                            random.SystemRandom().uniform(min(y) - alpha * dy, max(y) + alpha * dy)])
+            new_ind.append([random.SystemRandom().uniform(min(xy[i]) - alpha * dd[i], max(xy[i]) + alpha * dd[i])
+                            for i in range(len(ind[0]))])
         return new_ind
