163fb54cfc666d8431f4ec15d2f9b4dc4531b113
luc <luc@slon.lotnik.net>
2020-12-12 07:16:14 +0100

    Fixed Gaussian Uniform Crossover operators to handle cubic polynomials

--- crossover_operators/gaussianuniformcrossover.py
+++ crossover_operators/gaussianuniformcrossover.py
@@ -6,12 +6,14 @@ class GaussianUniformCrossover(CrossoverBase):
     datatype = 'real'
 
     def crossover_function(self, ind, **kwargs):
-        new_ind = []
+        new_ind = [[], []]
         alpha = random.SystemRandom().random()
         for i in range(len(ind[0])):
             distance = abs(ind[0][i] - ind[1][i])
             if random.SystemRandom().random() <= 0.5:
-                new_ind.append([ind[0][i] + alpha * distance / 3, ind[1][i] + alpha * distance / 3])
+                new_ind[0].append(ind[0][i] + alpha * distance / 3)
+                new_ind[1].append(ind[1][i] + alpha * distance / 3)
             else:
-                new_ind.append([ind[1][i] + alpha * distance / 3, ind[0][i] + alpha * distance / 3])
+                new_ind[0].append(ind[1][i] + alpha * distance / 3)
+                new_ind[1].append(ind[0][i] + alpha * distance / 3)
         return new_ind
