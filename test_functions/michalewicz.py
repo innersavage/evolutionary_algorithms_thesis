571b36f93297c4bb17d2da9a573745594c1091a1
luc <luc@slon.lotnik.net>
2021-01-18 15:58:53 +0100

    Oh shot... another mistake in test function :-(

--- test_functions/michalewicz.py
+++ test_functions/michalewicz.py
@@ -4,6 +4,6 @@ from .functionbase import TestFunction
 
 class Michalewicz(TestFunction):
     def test_function(self, xx, m=10):
-        f = - sum([np.sin(x) * np.float_power(np.sin(i * np.float_power(x, 2) / np.pi), 2 * m)
+        f = - sum([np.sin(x) * np.float_power(np.sin((i * np.float_power(x, 2)) / np.pi), 2 * m)
                    for i, x in enumerate(xx, start=1)])
         return f
